3d85c2878f33f5843ce44b4b92c13d6f1d461eae
Alexei A <alexey.alexandrenko@gmail.com>
2024-11-05 13:03:40 +0100

    Adds tests for basic response body

---
+++ src/test/java/restassured/BasicResponseBodyDemo.java
@@ -0,0 +1,60 @@
+package restassured;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+
+
+import java.util.Map;
+
+public class BasicResponseBodyDemo {
+
+    public static final String LIMIT_EP = "https://api.github.com/rate_limit";
+
+    @Test
+    public void jsonPathReturnsMap() {
+        Response response = RestAssured.get(LIMIT_EP);
+
+        ResponseBody<?> body = response.body();
+        JsonPath jPath = body.jsonPath();
+
+        JsonPath jPath2 = response.body().jsonPath();
+
+
+        Map<String, String> fullJson = jPath2.get();
+        Map<String, String> subMap = jPath2.get("resources");
+        Map<String, String> subMap2 = jPath2.get("resources.core");
+
+
+        int value = jPath2.get("resources.core.limit");
+        int value2 = jPath2.get("resources.graphql.remaining");
+
+
+        System.out.println(fullJson);
+        System.out.println(subMap);
+        System.out.println(subMap2);
+        System.out.println(value);
+        System.out.println(value2);
+
+        assertEquals(value, 60);
+        assertEquals(value2, 0);
+
+    }
+
+
+    @Test
+    public void castingFailure(){
+        JsonPath jPath = RestAssured.get(LIMIT_EP).body().jsonPath();
+
+        Map<String, String> isNull = jPath.get("incorrect.path"); // NPE
+        int aMap = jPath.get("resources.core"); //ClassCastException
+        String value = jPath.get("resources.core.limit"); //ClassCastException
+    }
+
+
+
+}
