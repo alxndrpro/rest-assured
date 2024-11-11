04d71a8b6b1eab1ad67acec462b14c9e494ea577
Alexei A <alexey.alexandrenko@gmail.com>
2024-11-11 17:20:52 +0100

    Adds basic logging example

---
+++ src/test/java/restassured/LoggingDemo.java
@@ -0,0 +1,27 @@
+package restassured;
+
+import groovy.util.logging.Log;
+import io.restassured.RestAssured;
+import io.restassured.filter.log.LogDetail;
+import org.testng.annotations.Test;
+
+public class LoggingDemo {
+
+    static final String POSTS_EP = "https://jsonplaceholder.typicode.com/posts";
+
+    @Test
+    void logTest() {
+        RestAssured
+                .given()
+                    .log().all()
+                    .log().headers()
+                    .log().body()
+                .when()
+                    .get(POSTS_EP)
+                .then()
+                    .log().headers()
+                    .log().ifValidationFails(LogDetail.ALL)
+                    .statusCode(200);
+    }
+
+}
