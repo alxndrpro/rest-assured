d04d6c8af62ed755bafe1a96eb7d575566a80777
Alexei A <alexey.alexandrenko@gmail.com>
2024-11-11 18:26:31 +0100

    Adds central config

---
+++ src/main/java/restassured/ConfigFactory.java
@@ -0,0 +1,29 @@
+package restassured;
+
+import io.restassured.RestAssured;
+import io.restassured.config.FailureConfig;
+import io.restassured.config.LogConfig;
+import io.restassured.config.RedirectConfig;
+import io.restassured.config.RestAssuredConfig;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.listener.ResponseValidationFailureListener;
+
+public class ConfigFactory {
+
+
+
+    public static RestAssuredConfig getDefaultConfig() {
+
+        ResponseValidationFailureListener failureListener = (reqSpec, resSpec, response) ->
+                System.out.printf("We have a failure, " +
+                                "response status was %s and the body contained: %s",
+                        response.getStatusCode(), response.body().asPrettyString());
+
+        return RestAssured.config()
+                .logConfig(LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails(LogDetail.ALL))
+                .redirect(RedirectConfig.redirectConfig().maxRedirects(1))
+                .failureConfig(FailureConfig.failureConfig().failureListeners(failureListener));
+    }
+
+
+}
